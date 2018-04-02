2c6b52a75ee07488b750448d066495402d8b9f20
josionramos <37560881+josionramos@users.noreply.github.com>
2018-04-02 20:25:31 -0300

    Update Product.php
    fix to the filter of  variants

--- server/app/Http/Resources/Product/Product.php
+++ server/app/Http/Resources/Product/Product.php
@@ -58,19 +58,39 @@ class Product extends Resource
             // Variants
             if (in_array('variants', $includes)) {
                 $data['variants'] = [];
+                //Variants da categoria
                 $variants = $this->category->variants;
-
+                //echo "<script>console.log( 'BEGIN------GET variants includes------' );</script>";
+                //echo "<script>console.log( 'Category size: " . $variants->count() . "' );</script>";
+                \Log::debug('Category size', [$variants->count()]);
                 if ($variants->count() > 0) {
+                    //Category
                     foreach ($variants as $variant) {
                         $key = str_slug($variant->name, '_');
-                        $item = $this->variants->where('type_id', $variant->id)->first();
-
-                        if ($item) {
-                            $data['variants'][$key][] = new Variant($item);
+                        \Log::debug('Category name', [$key]);
+                        //echo "<script>console.log( 'Category name: " . $key . "' );</script>";
+                        $prod_variantes = $this->variants;
+                        foreach ($prod_variantes as $prod_variant) {
+                            //echo "<script>console.log( 'Category ID: " . $key . "' );</script>";
+                            //echo "<script>console.log( 'Category ID from Prod: " . $prod_variant->type_id . "' );</script>";
+                            \Log::debug('Category ID', [$variant->id]);
+                            \Log::debug('Categoty from Prod ID', [$prod_variant->type_id]);
+                            if ($variant->id == $prod_variant->type_id) {
+                                $item = $prod_variant;
+                                //echo "<script>console.log( 'Prod name: " . $prod_variant->value . "' );</script>";
+                                \Log::debug('Prod item', [$item]);
+                                if ($item) {
+                                    $data['variants'][$key][] = new Variant($item);
+                                }
+                            }
                         }
+                        //$item = $this->variants->where('type_id', $variant->id)->first();
+                        //if ($item) {
+                        //    $data['variants'][$key][] = new Variant($item);
+                        //}
                     }
                 }
-
+                // echo "<script>console.log( 'END------GET variants includes------' );</script>";
                 // $data['variants'] = Variant::collection($this->variants);
             }
 
